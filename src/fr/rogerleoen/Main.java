362eca00d49e7715e2c5ba54f451308ac7cb73d9
Jean-Baptiste <roger.jeanbaptiste@gmail.com>
2016-04-26 10:24:52 +0200

    Hello world

--- src/fr/rogerleoen/Main.java
+++ src/fr/rogerleoen/Main.java
@@ -1,7 +1,7 @@
 package fr.rogerleoen;
 
 public class Main {
-
+    //// TODO: 26/04/2016  : mettre en palce la vie
     public static void main(String[] args) {
         System.out.println("hello World");
     }
