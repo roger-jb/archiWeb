90d9441cafd32407b200a97d4cef726848983b8f
Jean-Baptiste <roger.jeanbaptiste@gmail.com>
2016-04-26 10:48:29 +0200

    mise en place de l'architecture

---
+++ src/fr/rogerleoen/aspirateur/Main.java
@@ -0,0 +1,7 @@
+package fr.rogerleoen.aspirateur;
+
+/**
+ * Created by Jean-Baptiste on 26/04/2016.
+ */
+public class Main {
+}
---
+++ src/fr/rogerleoen/integrateur/Main.java
@@ -0,0 +1,7 @@
+package fr.rogerleoen.integrateur;
+
+/**
+ * Created by Jean-Baptiste on 26/04/2016.
+ */
+public class Main {
+}
